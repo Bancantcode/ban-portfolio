5885709e093bd656b09aac02fedab05e5d381594
Bancantcode <santiagobryan831@gmail.com>
2025-02-10 18:31:15 +0800

    configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,39 @@ export default {
   ],
   theme: {
     extend: {
+      fontSize: {
+        "base-small": "clamp(0.821rem, 0.3071rem + 0.4647vi, 0.8943rem)",
+        "base": "clamp(0.9rem, 0.9rem + 0.07vw, 1.05rem)",
+        "base-large": "clamp(1.1rem, 0.7071rem + 0.5143vi, 1.4rem)",
+        'heading-6': 'clamp(1rem, 0.928rem + 0.36vw, 1.25rem)',
+        "heading-5": "clamp(1.2rem, 1rem + 0.45vw, 1.57rem)",
+        "heading-4": "clamp(1.4rem, 0.9rem + 0.5vw, 1.5rem)",
+        "heading-3": "clamp(1.5625rem, 1.2474rem + 1.5755vi, 2.6653rem)",
+        "heading-2": "clamp(3rem, 1.496rem + 2.2854vi, 3.5529rem)",
+        "heading-1": "clamp(4.688rem, 2.25rem + 1vw, 4.6875rem)",
+        "heading-display": "clamp(2.6518rem, 1.88rem + 3.459vi, 7.3131rem)",
+      },
+      spacing: {
+        "3xs": "clamp(0.3125rem, 0.3125rem + 0vi, 0.3125rem)",
+        "2xs": "clamp(0.5625rem, 0.5446rem + 0.0893vi, 0.625rem)",
+        "xs": "clamp(0.875rem, 0.8571rem + 0.0893vi, 0.9375rem)",
+        "sm": "clamp(1.125rem, 1.0893rem + 0.1786vi, 1.25rem)",
+        "md": "clamp(1.6875rem, 1.6339rem + 0.2679vi, 1.875rem)",
+        "lg": "clamp(2.25rem, 2.1786rem + 0.3571vi, 2.5rem)",
+        "xl": "clamp(3.375rem, 3.2679rem + 0.5357vi, 3.75rem)",
+        "2xl": "clamp(4.5rem, 4.3571rem + 0.7143vi, 5rem)",
+        "3xl": "clamp(5.75rem, 6.2571rem + 1.2023vi, 7.25rem)",
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        dark: "var(--dark)",
+        concrete: "var(--concrete)",
+        olive: "var(--olive)",
+        beige: "var(--beige)",
+      },
+      fontFamily: {
+        regular: "var(--text-regular)",
+        medium: "var(--text-medium)",
+        // italic: "var(--text-italic)"
       },
     },
   },
