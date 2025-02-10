0196017e3663c590a1334fb597d34149cd1bafff
Bancantcode <santiagobryan831@gmail.com>
2025-02-10 18:20:31 +0800

    hooks and constant folder

---
+++ src/constants/constants.ts
@@ -0,0 +1,32 @@
+export const ProjectInfo = [
+    {
+        role: '',
+        image: '',
+        name: '',
+        description: ''
+    },
+    {
+        role: '',
+        image: '',
+        name: '',
+        description: ''
+    },
+    {
+        role: '',
+        image: '',
+        name: '',
+        description: ''
+    },
+    {
+        role: '',
+        image: '',
+        name: '',
+        description: ''
+    },
+    {
+        role: '',
+        image: '',
+        name: '',
+        description: ''
+    },
+];
