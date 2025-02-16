29455d2c52ccf89680265998a880f84552a1db67
Bancantcode <santiagobryan831@gmail.com>
2025-02-16 18:34:34 +0800

    vercel check

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,12 @@
-'use client'
+// 'use client'
 import TextHeading from "@/components/ui/TextHeading"
 import Button from "@/components/ui/Button"
 import Skills from "@/components/ui/Skills"
 import Certificates from "@/components/ui/Certificates"
-import useLenisScroll from "@/hooks/useLenisScroll"
+// import useLenisScroll from "@/hooks/useLenisScroll"
 
 const AboutPage = () => {
-  useLenisScroll();
+  // useLenisScroll();
   return (
     <main>
       <section>
