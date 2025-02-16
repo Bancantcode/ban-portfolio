743d016f835e57f3a5395418c0b317816d2122da
Bancantcode <santiagobryan831@gmail.com>
2025-02-16 18:11:30 +0800

    text heading responsive

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,13 +1,13 @@
-'use client'
+// 'use client'
 import { navLinks } from "@/constants/constants"
 import Button from "../ui/Button"
 import Logo from '../../../public/images/SB-logo.png'
 import Image from "next/image"
-import useGsapAnimations from "@/hooks/useGsapAnimations"
+// import useGsapAnimations from "@/hooks/useGsapAnimations"
 import Link from "next/link"
 
 const Navbar = () => {
-  useGsapAnimations();
+  // useGsapAnimations();
   return (
     <header className="flex justify-between items-center py-xs md:py-md section-x-padding">
       <div className="overflow-hidden">
@@ -15,7 +15,7 @@ const Navbar = () => {
           <Image className="" src={Logo} alt="Banwagon Logo" width={45} height={40} id="scroll-effect"/>
         </Link>
       </div>
-      <div className="flex gap-1">
+      <div className="hidden md:flex gap-1">
         {
           navLinks.map(link => (
             <a key={link.title} href={link.href} className="">
--- src/components/ui/TextHeading.tsx
+++ src/components/ui/TextHeading.tsx
@@ -1,7 +1,7 @@
 'use client'
 import TextReveal from "./TextReveal"
 import Image from "next/image";
-import useGsapAnimations from "@/hooks/useGsapAnimations";
+// import useGsapAnimations from "@/hooks/useGsapAnimations";
 import Glow1 from '../../../public/images/glow/Glow1.webp'
 
 interface TextHeadingProps {
@@ -10,12 +10,12 @@ interface TextHeadingProps {
 }
 
 const TextHeading: React.FC<TextHeadingProps> = ({ text, description }) => {
-  useGsapAnimations();
+  // useGsapAnimations();
   return (
     <div className="grid-style section-x-padding py-xl md:py-3xl">
-      <Image src={Glow1} alt="Glow1" className="absolute inset-0 -z-10" id="glow__animation1" loading="lazy" />
-      <TextReveal text={text} className="text-heading-3 lg:text-heading-1 leading-none col-start-1 col-span-12 mb-xl" marginRight="mr-2"/>
-      <TextReveal text={description} className="text-base col-start-1 col-span-4 lg:col-start-9 lg:col-span-5" marginRight="mr-1" />
+      <Image src={Glow1} alt="Glow1" className="absolute inset-0 -z-10" id="glow__animation1" />
+      <TextReveal text={text} className="heading leading-none col-start-1 col-span-12 mb-xl" marginRight="mr-2"/>
+      <TextReveal text={description} className="text-base-small md:text-base leading-tight opacity-60 col-start-1 col-span-12 md:col-start-6 md:col-span-5 lg:col-start-7 lg:col-span-6 xl:col-start-9 xl:col-span-4" marginRight="mr-1" />
     </div>
   )
 }
