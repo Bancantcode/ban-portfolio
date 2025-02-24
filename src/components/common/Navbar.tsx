769a52ef47a26cb242d8a6d66621f16bd883ca79
Bancantcode <santiagobryan831@gmail.com>
2025-02-24 14:45:38 +0800

    usegsapanimation

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,13 +1,13 @@
-// 'use client'
+'use client'
 import { navLinks } from "@/constants/constants"
 import Button from "../ui/Button"
 import Logo from '../../../public/images/SB-logo.png'
 import Image from "next/image"
-// import useGsapAnimations from "@/hooks/useGsapAnimations"
+import useGsapAnimations from "@/hooks/useGsapAnimations"
 import Link from "next/link"
 
 const Navbar = () => {
-  // useGsapAnimations();
+  useGsapAnimations();
   return (
     <header className="flex justify-between items-center py-xs md:py-md section-x-padding">
       <div className="overflow-hidden">
@@ -18,9 +18,9 @@ const Navbar = () => {
       <div className="hidden md:flex gap-1">
         {
           navLinks.map(link => (
-            <a key={link.title} href={link.href} className="">
+            <Link key={link.title} href={link.href} className="">
               <Button id="nav-btn" title={link.title} containerClass="" />
-            </a>
+            </Link>
           ))
         }
       </div>
