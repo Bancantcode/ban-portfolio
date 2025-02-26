58e962e2bab8797594184e2eb8c44a3b2c161cd6
Bancantcode <santiagobryan831@gmail.com>
2025-02-26 23:35:01 +0800

    hide about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -10,7 +10,10 @@ const AboutPage = () => {
   useLenisScroll();
   return (
     <main>
-      <section>
+      <div className="flex justify-center items-center h-screen">
+        <h1>Hellar</h1>
+      </div>
+      {/* <section>
         <TextHeading 
           text="About me — I'm Bryan, a freelance web developer dedicated to crafting digital experiences for businesses to unlock their growth potential." 
           description={`I have been passionate about design since I was in school, and I currently specialize in creating custom websites that I genuinely love. I have a strong desire to push the limits of creativity and honor excellence in the field of digital design. My focus is on providing specialized digital solutions, fusing creativity with artistic vision to bring my clients' vision to life. I'm supposed to go above and beyond expectations with every endeavor and make a lasting impact.`} 
@@ -29,10 +32,7 @@ const AboutPage = () => {
 
       <section>
         <Certificates />
-      </section>
-
-
-
+      </section> */}
     </main>
   )
 }
