773dc13697f12cf989c3a0b9805ce3bca475de54
Bancantcode <santiagobryan831@gmail.com>
2025-02-26 23:36:52 +0800

    hide about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,9 @@
 'use client'
-import TextHeading from "@/components/ui/TextHeading"
-import Stats from "@/components/ui/Stats"
-import WhoAmI from "@/components/ui/WhoAmI"
-import Skills from "@/components/ui/Skills"
-import Certificates from "@/components/ui/Certificates"
+// import TextHeading from "@/components/ui/TextHeading"
+// import Stats from "@/components/ui/Stats"
+// import WhoAmI from "@/components/ui/WhoAmI"
+// import Skills from "@/components/ui/Skills"
+// import Certificates from "@/components/ui/Certificates"
 import useLenisScroll from "@/hooks/useLenisScroll"
 
 const AboutPage = () => {
